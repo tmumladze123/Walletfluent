d7b5d9573db34b779fc1469aa19c74902f3b7c75
nika <nikoloz.latsabidze.1@btu.edu.ge>
2021-12-26 20:58:53 +0400

    calculator/converter

--- WalletFluent/app/src/main/java/ge/nlatsabidze/walletfluent/ui/crypto/CryptoViewModel.kt
+++ WalletFluent/app/src/main/java/ge/nlatsabidze/walletfluent/ui/crypto/CryptoViewModel.kt
@@ -1,15 +1,13 @@
 package ge.nlatsabidze.walletfluent.ui.crypto
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ge.nlatsabidze.walletfluent.model.cryptoModel.Exchanges
-import ge.nlatsabidze.walletfluent.model.valuteModel.Converter
 import ge.nlatsabidze.walletfluent.network.cryptoNetwork.CryptoRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -21,12 +19,23 @@ class CryptoViewModel @Inject constructor(private val cryptoService: CryptoRepos
     private val _cryptoExchangedValues = MutableSharedFlow<List<Exchanges>>()
     val cryptoExchangedValues: MutableSharedFlow<List<Exchanges>> get() = _cryptoExchangedValues
 
+    private var _showLoadingViewModelState = MutableStateFlow<Boolean>(false)
+    val showLoadingViewModel: MutableStateFlow<Boolean> get() = _showLoadingViewModelState
+
     fun getCryptoExchangeValues() {
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
                 cryptoService.getCryptoExchanges().collectLatest {
                     _cryptoExchangedValues.emit(it.data!!)
                 }
+            }
+        }
+    }
+
+    fun showLoadingBar() {
+        viewModelScope.launch {
+            cryptoService.showLoading.collectLatest {
+                _showLoadingViewModelState.value = it
             }
         }
     }
--- WalletFluent/app/src/main/java/ge/nlatsabidze/walletfluent/ui/currency/currencyPages/calculatorCurrencies/CalculatorPageFragment.kt
+++ WalletFluent/app/src/main/java/ge/nlatsabidze/walletfluent/ui/currency/currencyPages/calculatorCurrencies/CalculatorPageFragment.kt
@@ -1,20 +1,17 @@
 package ge.nlatsabidze.walletfluent.ui.currency.currencyPages.calculatorCurrencies
 
-import android.text.Editable
 import android.util.Log.d
-import android.view.WindowManager
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import ge.nlatsabidze.walletfluent.BaseFragment
 import ge.nlatsabidze.walletfluent.R
 import ge.nlatsabidze.walletfluent.databinding.CalculatorPageFragmentBinding
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import android.widget.AutoCompleteTextView
-import android.widget.AdapterView
-import android.widget.AdapterView.OnItemClickListener
 
 
 @AndroidEntryPoint
@@ -24,6 +21,7 @@ class CalculatorPageFragment :
     private val calculatorViewModel: CalculatorPageViewModel by viewModels()
 
     override fun start() {
+        makeResultEditTextNotClickable()
         collectConvertedValue()
         setValueToResult()
     }
@@ -31,63 +29,56 @@ class CalculatorPageFragment :
     private fun collectConvertedValue() {
 
         val countries = resources.getStringArray(R.array.countries)
-//        val value = binding.autoCompleteFrom.text.toString()
 
         val countriesFrom = ArrayAdapter(requireContext(), R.layout.dropdown_item, countries)
         binding.autoCompleteFrom.setAdapter(countriesFrom)
         binding.autoCompleteTo.setAdapter(countriesFrom)
 
         var firstValue = binding.textInputLayoutWrapper.editText?.text.toString()
+        var secondValue = binding.secondTextInputLayoutWrapper.editText?.text.toString()
         binding.autoCompleteFrom.onItemClickListener =
             OnItemClickListener { adapterView, view, position, id ->
                 val selectedValue: String? = countriesFrom.getItem(position)
                 firstValue = selectedValue!!
+                if (binding.etNumber.text?.isNotEmpty() == true) {
+                    calculatorViewModel.getConvertedValue(binding.etNumber.text.toString().toDouble(), firstValue, secondValue)
+                }
                 d("dsadsa", firstValue)
             }
 
-        var secondValue = binding.secondTextInputLayoutWrapper.editText?.text.toString()
+
         binding.autoCompleteTo.onItemClickListener =
             OnItemClickListener { adapterView, view, position, id ->
                 val selectedValue: String? = countriesFrom.getItem(position)
                 secondValue = selectedValue!!
+                if (binding.etNumber.text?.isNotEmpty() == true) {
+                    calculatorViewModel.getConvertedValue(binding.etNumber.text.toString().toDouble(), firstValue, secondValue)
+                }
                 d("dsadsa", secondValue)
             }
 
-        binding.btnConverter.setOnClickListener {
-            if (binding.etNumber.text.toString().isNotEmpty()) {
+        binding.etNumber.doAfterTextChanged {
+            val amountAsString = binding.etNumber.text.toString()
+            if (amountAsString.isNotEmpty()) {
                 val amount = binding.etNumber.text.toString().toDouble()
-                d("amount", amount.toString())
                 calculatorViewModel.getConvertedValue(amount, firstValue, secondValue)
+            } else if (amountAsString.isEmpty() && binding.etConvertedNumber.text.toString().isNotEmpty()) {
+                binding.etConvertedNumber.text?.clear()
             }
         }
-
-        binding.btnSwap.setOnClickListener {
-            firstValue = secondValue.also { secondValue = firstValue }
-
-            binding.textInputLayoutWrapper.editText?.setText(firstValue)
-            binding.secondTextInputLayoutWrapper.editText?.setText(secondValue)
-
-//            val newCountries = ArrayAdapter(requireContext(), R.layout.dropdown_item, countries)
-//            binding.autoCompleteFrom.setAdapter(newCountries)
-//            binding.autoCompleteTo.setAdapter(newCountries)
-            binding.autoCompleteFrom.showDropDown()
-            binding.autoCompleteTo.showDropDown()
-
-//            binding.autoCompleteFrom.onItemClickListener =
-//                OnItemClickListener { adapterView, view, position, id ->
-//                    val selectedValue: String? = countriesFrom.getItem(position)
-//                    secondValue = selectedValue!!
-//                    d("dsadsa", secondValue)
-//                }
-        }
-
     }
 
     private fun setValueToResult() {
         viewLifecycleOwner.lifecycleScope.launch {
-            calculatorViewModel.convertedValue.collectLatest {
-                binding.etConvertedNumber.setText(it.value.toString())
+            calculatorViewModel.convertedValue.collect {
+                if (binding.etNumber.text?.isNotEmpty() == true) {
+                    binding.etConvertedNumber.setText(it.value.toString())
+                }
             }
         }
+    }
+
+    private fun makeResultEditTextNotClickable() {
+        binding.etConvertedNumber.isEnabled = false
     }
 }

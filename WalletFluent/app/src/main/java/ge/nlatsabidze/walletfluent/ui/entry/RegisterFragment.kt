d6d4adc9012d36bc44848a143bceb2b5bd172fc3
nika <nikoloz.latsabidze.1@btu.edu.ge>
2021-12-27 23:07:40 +0400

    realtime db

--- WalletFluent/app/src/main/java/ge/nlatsabidze/walletfluent/ui/entry/RegisterFragment.kt
+++ WalletFluent/app/src/main/java/ge/nlatsabidze/walletfluent/ui/entry/RegisterFragment.kt
@@ -2,16 +2,20 @@ package ge.nlatsabidze.walletfluent.ui.entry
 
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import dagger.hilt.android.AndroidEntryPoint
 import ge.nlatsabidze.walletfluent.BaseFragment
 import ge.nlatsabidze.walletfluent.R
 import ge.nlatsabidze.walletfluent.databinding.FragmentRegisterBinding
 import ge.nlatsabidze.walletfluent.extensions.showDialogError
+import ge.nlatsabidze.walletfluent.ui.entry.userData.User
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
@@ -21,15 +25,17 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding>(FragmentRegisterB
 
     private lateinit var firebaseAuth: FirebaseAuth
     private val loginViewModel: LoginRegisterViewModel by activityViewModels()
+    private lateinit var database: DatabaseReference
 
 
     override fun start() {
 
         firebaseAuth = FirebaseAuth.getInstance()
+        binding.btnSignUp.setOnClickListener {
+            registerUser()
+        }
 
-        binding.btnSignUp.setOnClickListener { registerUser() }
-
-        listeners()
+        observervers()
     }
 
     private fun registerUser() {
@@ -41,7 +47,7 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding>(FragmentRegisterB
         }
     }
 
-    private fun listeners() {
+    private fun observervers() {
         viewLifecycleOwner.lifecycleScope.launch {
             loginViewModel.userMutableLiveFlow.collect { userLogedIn ->
                 if (userLogedIn) {
@@ -105,4 +111,5 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding>(FragmentRegisterB
         val builder = AlertDialog.Builder(requireContext())
         builder.showDialogError(message, requireContext())
     }
+
 }
